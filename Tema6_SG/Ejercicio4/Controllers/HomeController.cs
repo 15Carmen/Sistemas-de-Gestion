489d556dc3e3131ff4b8c6b603a9f734575e2062
15Carmen <carmen.martin@iesnervion.es>
2023-10-19 11:54:09 +0200

    van saliendo las cosas :D

---
+++ Tema6_SG/Ejercicio4/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Ejercicio4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
