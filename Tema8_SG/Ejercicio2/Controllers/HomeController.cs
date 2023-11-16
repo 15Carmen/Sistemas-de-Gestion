e33a4cae4cf2c4ed5ab514e2bbca817b1ef3938d
15Carmen <carmen.martin@iesnervion.es>
2023-11-16 10:01:09 +0100

    Ejercicios1 y 2 hechos

---
+++ Tema8_SG/Ejercicio2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Ejercicio2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Action de la primera vez que el usuario pide la página.
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Action cuando el usuario ha pulsado el botón.
+        [HttpPost]
+        public IActionResult IndexPost(string nombre)
+        {
+            ViewBag.nombre = nombre;
+
+            
+             //Esto devuelve la vista Index, la misma vista deonde recogemos los datos
+             return View();
+            
+
+            //Esto devuelve la vista de IndexPost
+            //return View("IndexPost");
+        }
+    }
+}
--- Tema8_SG/Tema8_SG/Controllers/HomeController.cs
+++ Tema8_SG/Tema8_SG/Controllers/HomeController.cs
@@ -6,27 +6,16 @@ namespace Tema8_SG.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+       public IActionResult Saludo(string nombre)
         {
+            ViewBag.nombre = nombre;
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
