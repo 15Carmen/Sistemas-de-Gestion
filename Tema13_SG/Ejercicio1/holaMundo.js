7b23db6659243403dd908e5c6a6aabb28eef317c
15Carmen <carmen.martin@iesnervion.es>
2024-01-18 14:21:55 +0100

    cositas

---
+++ Tema13_SG/Ejercicio1/holaMundo.js
@@ -0,0 +1,26 @@
+window.onload = inicializaEvento
+
+function inicializaEvento(){
+    let button = document.getElementById("btnSaludar");
+
+    button.addEventListener("click", saludar, false);
+}
+
+function saludar(){
+    let miLlamada = new XMLHttpRequest();
+
+    let divMensaje = document.getElementById("divMensaje")
+
+    miLlamada.open("GET", "https://crudnervion.azurewebsites.net/api/personas")
+
+    miLlamada.onreadystatechange = function(){
+        if(miLlamada.readyState < 4){
+            divMensaje.innerHTML = "Cargando..."
+        }else if (miLlamada.readyState==4 && miLlamada.status == 200){
+            let response = JSON.parse(miLlamada.responseText);
+            divMensaje.innerHTML = response[1].nombre + " " + response[1].apellidos;
+        }
+    };
+
+    miLlamada.send();
+}
